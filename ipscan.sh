f0ab9ee6c77ac856135e6107b5f993895b0036b6
Aaron Haymore <52760493+Eromyah@users.noreply.github.com>
2020-07-17 03:28:07 -0400

    Update ipscan.sh

--- ipscan.sh
+++ ipscan.sh
@@ -1,5 +1,5 @@
 for ip in $(seq 1 5); do
-        ping -c 172.18.0.$ip > /dev/null & echo "Online: 172.18.0.$ip"
+        ping -c 1 172.18.0.$ip > /dev/null & echo "Online: 172.18.0.$ip";
 done
 
 what this does is it will ping the ip address and if it recevies a error it will not reach the & but if it does not error it will continue on and print out a success
