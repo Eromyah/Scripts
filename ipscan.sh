1e8e3cd475bac5728d00a2ebca19fe9ca50c3552
Aaron Haymore <52760493+Eromyah@users.noreply.github.com>
2020-07-17 03:28:26 -0400

    Update ipscan.sh

--- ipscan.sh
+++ ipscan.sh
@@ -2,5 +2,5 @@ for ip in $(seq 1 5); do
         ping -c 1 172.18.0.$ip > /dev/null & echo "Online: 172.18.0.$ip";
 done
 
-what this does is it will ping the ip address and if it recevies a error it will not reach the & but if it does not error it will continue on and print out a success
-replace my 172.18.0 with your correct subnet address
+#what this does is it will ping the ip address and if it recevies a error it will not reach the & but if it does not error it will continue on and print out a success
+#replace my 172.18.0 with your correct subnet address
